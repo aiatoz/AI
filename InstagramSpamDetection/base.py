b8bb715fda97028aebbbf76ff00d6cb17fa25c1e
aiatoz <125128688+aiatoz@users.noreply.github.com>
2023-10-12 17:42:44 +0530

    Create base.py

---
+++ InstagramSpamDetection/base.py
@@ -0,0 +1,23 @@
+#Instagram spam detector
+
+#Post/Comment/Account Detector
+
+'''
+Feature extraction for training
+Post
+  -Account Tags
+  -Timing
+  -Hash Tags
+  -Words
+  -Links
+  -Images
+Account
+  -Spam followers
+  -Spam accounts followed
+  -Posts
+  -Spam reports
+  -Comments on other posts
+  -Email accounts (Domains)
+Misc
+  -Similar posts in small time frame
+'''
